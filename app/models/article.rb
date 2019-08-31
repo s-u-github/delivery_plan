ee09ee56dcc7870fcb9b5e13349f0d8555181d61
shoutaro.u <overture21495@gmail.com>
2019-08-31 15:30:38 +0000

    i18n バリデーション見直し

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,15 +1,15 @@
 class Article < ApplicationRecord
   
   belongs_to :user
-  has_many :DailyReports
+  has_many :DailyReports, dependent: :delete_all
   geocoded_by :address
   after_validation :geocode
   
-  # validates :title, presence: true
-  # validates :postcode, presence: true, 
-  #   format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/, message: "が適切ではありません"}
-  # validates :address, presence: true
-  # validates :phone_num, presence: true, format: { with: /\A\d{10,11}\z/, message: "が適切ではありません"}
+  validates :title, presence: true
+  validates :postcode, presence: true, 
+    format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/, message: "が適切ではありません"}
+  validates :address, presence: true
+  validates :phone_num, presence: true, format: { with: /\A\d{10,11}\z/, message: "が適切ではありません"}
   
   
 end
