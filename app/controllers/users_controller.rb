d17aca96ab2d7dfa96c2e538ad5936018f4333e5
shoutaro.u <overture21495@gmail.com>
2019-09-04 08:01:39 +0000

    user edit update add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,12 @@ class UsersController < ApplicationController
   # 正しいユーザーでなければ実行できない
   before_action :correct_user,   only: [:show]
   
+  # ユーザー登録ページ
   def new
     @user = User.new
   end
   
+  # ユーザー登録
   def create
     @user = User.new(users_params)
     if @user.save
@@ -19,36 +21,50 @@ class UsersController < ApplicationController
     end
   end
   
+  # ユーザー編集
+  def edit
+    @user = User.find(current_user.id)
+  end
+  
+  # ユーザー更新
+  def update
+    @user = User.find(current_user.id)
+    if @user.update_attributes(users_params)
+      flash[:success] = "ユーザー情報を更新しました。"
+      redirect_to user_path(current_user.id)
+    end
+    
+  end
+  
   def show
     @user = User.find(params[:id])
-    @base = @user.articles.find_by(base_point: true)
   end
   
   
   
   private
   
-  def users_params
-    params.require(:user).permit(:name, :email, :phone_num, :password, :password_confirmation)
-  end
-  
-  # beforeアクション
-  
-  # ログイン済みユーザーか確認
-  def logged_in_user
-    unless logged_in?
-      # sessionsヘルパーメソッド
-      store_location
-      flash[:danger] = "ログインしてください。"
-      redirect_to login_url
+    def users_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
-  end
-  
-  # 正しいユーザーかどうか確認
-  def correct_user
-    @user = User.find(params[:id])
-    # 後付けif文の構成と一緒で、条件式がfalseの場合のみ、冒頭のコードが実行される、current_user?はsessonヘルパーメソッド
-      redirect_to(root_url) unless current_user?(@user)
-  end
+    
+    # beforeアクション
+    
+    # ログイン済みユーザーか確認
+    def logged_in_user
+      unless logged_in?
+        # sessionsヘルパーメソッド
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
+    end
+    
+    # 正しいユーザーかどうか確認
+    def correct_user
+      @user = User.find(params[:id])
+      # 後付けif文の構成と一緒で、条件式がfalseの場合のみ、冒頭のコードが実行される、current_user?はsessonヘルパーメソッド
+        redirect_to(root_url) unless current_user?(@user)
+    end
   
 end
