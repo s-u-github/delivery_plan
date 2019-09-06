08ea393af721ed3bef2df1cf6d0fa072c3fc0df1
shoutaro.u <overture21495@gmail.com>
2019-09-06 10:57:38 +0000

    postcode,phone_num validates add

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,11 +4,10 @@ class Article < ApplicationRecord
   has_many :DailyReports, dependent: :delete_all
   geocoded_by :address
   after_validation :geocode
-  
   validates :title, presence: true
-  validates :postcode, presence: true
+  validates :postcode, presence: true, format: { with: /\A\d{3}[-]\d{4}$|^\d{3}[-]\d{2}$|^\d{3}$|^\d{5}$|^\d{7}\z/}
   validates :address, presence: true
-  validates :phone_num, presence: true
+  validates :phone_num, presence: true, format: { with: /\A[0-9-]{,14}\z/ }
   
   
 end
